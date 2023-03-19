21a2454563121d3f9779265da61374293ec37f35
M-Kulinkovich <wait_z@mail.ru>
2023-03-19 12:44:19 +0300

    connect to DB and create migrations

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+USER_DB = os.environ.get('USER_DB')
+PASS_DB = os.environ.get('PASS_DB')
+HOST_DB = os.environ.get('HOST_DB')
+PORT_DB = os.environ.get('PORT_DB')
+NAME_DB = os.environ.get('NAME_DB')
